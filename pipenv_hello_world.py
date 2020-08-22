24f472c22edc6d5c226493a669abee48c830db15
JosueDLA <josuedlavs@gmail.com>
2020-08-22 02:40:19 -0600

    Added Hello World.py

---
+++ pipenv_hello_world.py
@@ -0,0 +1,10 @@
+import requests
+
+print("Hello Worlds")
+
+response = requests.get('https://api.github.com')
+
+if response.status_code == 200:
+    print('Success!')
+elif response.status_code == 404:
+    print('Not Found.')
